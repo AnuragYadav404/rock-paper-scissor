1046d8c54d9220d2837d9d4a7631c3c6a690d30f
starboy <124185505+new-Begginings@users.noreply.github.com>
2023-02-19 23:10:46 +0530

    Update index.html styles.css and javascript.js
    Completed the layout of index.html file.
    Completed the javascript.js file with winner being declared
    using alert function.
    Added styling properties to index.html layout and buttons.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+const pscore = document.querySelector('.playerScore');
+const cscore = document.querySelector('.computerScore');
+console.log(pscore.innerText);
+console.log(cscore.innerText)
+
+
+function result() {
+    alert(`FINAL SCORE : \nPlayer score : ${pscore.innerText}\nComputer score : ${cscore.innerText}`);
+    pscore.innerText = 0;
+    cscore.innerText = 0;
+}
+
+function incrementPlayer() {
+    pscore.innerText = +pscore.innerText+1;
+}
+
+function incrementComputer() {
+    cscore.innerText = +cscore.innerText+1;
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() *max);
@@ -12,57 +31,83 @@ function getComputerChoice() {
     return arr[randIndex];
 }
 
-function roundPlay(playerSelection, computerSelection) {
+function roundPlay(e) {
 
-    playerSelection.toUpperCase();
-    computerSelection.toUpperCase();
+    const playerSelection = this.classList.value;
+    const computerSelection = getComputerChoice();
+    
     if(playerSelection == "ROCK" && computerSelection == "SCISSOR") {
-        return 1;
+        incrementPlayer();
     }else if(playerSelection == "ROCK" && computerSelection == "PAPER") {
-        return -1;
+        incrementComputer();
     }else if(playerSelection == "PAPER" && computerSelection == "SCISSOR") {
-        return -1;
+        incrementComputer();
     }else if(playerSelection == "PAPER" && computerSelection == "ROCK") {
-        return 1;
+        incrementPlayer();
     }else if(playerSelection == "SCISSOR" && computerSelection == "ROCK") {
-        return -1;
+        incrementComputer();
     }else if(playerSelection == "SCISSOR" && computerSelection == "PAPER") {
-        return 1;
-    }else {
-        return 0;
+        incrementPlayer();
     }
 
+    if(pscore.innerText == '5' || cscore.innerText == '5') {
+        console.log("ass")
+        result();
+    }
+
+    
 }
 
-function game() {
-    let userScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i<5; i++) {
-        // let playerSelection = prompt("Enter your choice : Rock, Paper or Scissor?", "ROCK");
-        // let computerSelection = getComputerChoice();
-        // let result = roundPlay(playerSelection, computerSelection);
-        // if(result == 1){
-        //     console.log("user wins the round : ",i)
-        //     userScore++;
-        // }else if(result == -1) {
-        //     console.log("computer wins the round : ",i)
-        //     computerScore++;
-        // }else { 
-        //     console.log("round : ", i, " is a draw");
-        // }
-    }
-    // if(userScore > computerScore) {
-    //     console.log("User is the winner, with score : ",userScore);
-    // }else if(computerScore > userScore) {
-    //     console.log("Computer is the winner, with score : ", computerScore);
-    // }else { 
-    //     console.log("Its a draw");
-    // }
-}
+
+const btns = document.querySelectorAll('button');
+btns.forEach((btn)=> {
+    btn.addEventListener('click', roundPlay);
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function game() {
+//     let userScore = 0;
+//     let computerScore = 0;
+//     for(let i = 0; i<5; i++) {
+//         let playerSelection = prompt("Enter your choice : Rock, Paper or Scissor?", "ROCK");
+//         let computerSelection = getComputerChoice();
+//         let result = roundPlay(playerSelection, computerSelection);
+//         if(result == 1){
+//             console.log("user wins the round : ",i)
+//             userScore++;
+//         }else if(result == -1) {
+//             console.log("computer wins the round : ",i)
+//             computerScore++;
+//         }else { 
+//             console.log("round : ", i, " is a draw");
+//         }
+//     }
+//     if(userScore > computerScore) {
+//         console.log("User is the winner, with score : ",userScore);
+//     }else if(computerScore > userScore) {
+//         console.log("Computer is the winner, with score : ", computerScore);
+//     }else { 
+//         console.log("Its a draw");
+//     }
+// }
 
 // const playerSelection = "ROCK";
 // const computerSelection = getComputerChoice();
 // console.log(roundPlay(playerSelection, computerSelection));
 
-game();
+// game();
 
